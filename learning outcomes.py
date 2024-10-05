b345c0ee66b7f3634209d42792dcd738ef8bb163
Tharindu Dakshina <tharindudakshina2004@gmail.com>
2024-10-05 23:33:50 +0530

    Update learning outcomes.py

--- learning outcomes.py
+++ learning outcomes.py
@@ -1,1 +1,5 @@
-
+#The import statment gives us access class to
+#The functionality of the datatime class
+import datetime
+#today is a funtion that returns today's date
+print(datetime.date.today());
