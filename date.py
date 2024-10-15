8aad93b1ee6786629fd16ddae589d516d3cd0871
Tharindu Dakshina <tharindudakshina2004@gmail.com>
2024-10-15 22:06:59 +0530

    Update date.py

--- date.py
+++ date.py
@@ -8,7 +8,7 @@ print(currentDate.month);
 print(currentData.day);
 
 #In python we use strftime to format dates
-import datetime
-currentDate = datetime.date.today();
-# ///////////////////////////////////////////////////////////////
-print(current.Date.strftime('%d %b,%Y'));
+# import datetime
+# currentDate = datetime.date.today();
+# # ///////////////////////////////////////////////////////////////
+# print(current.Date.strftime('%d %b,%Y'));
